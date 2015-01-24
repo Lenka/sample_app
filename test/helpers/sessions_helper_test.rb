69f4e47292c3ae7c023db98be6d487f1ca5897c2
Lenka Lakomá <lenka@closethedoor.net>
2015-01-24 15:01:02 +0100

    Finish log in/log out

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -1,30 +1,19 @@
-module SessionsHelper
+require 'test_helper'
 
-  # Logs in the given user.
-  def log_in(user)
-    session[:user_id] = user.id
+class SessionsHelperTest < ActionView::TestCase
+
+  def setup
+    @user = users(:lenka)
+    remember(@user)
   end
 
-  # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
   end
 
-  # Returns the current logged-in user (if any).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  # Returns true if the user is logged in, false otherwise.
-  def logged_in?
-    !current_user.nil?
-  end
-
-  # Logs out the current user.
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
   end
 end
